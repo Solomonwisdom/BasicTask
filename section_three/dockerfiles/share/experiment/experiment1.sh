1282c70981ed6ea05f28976c85949f289ef030ab
Solomonwisdom <whg19962015@163.com>
2018-11-18 21:01:38 +0800

    just push

--- section_three/dockerfiles/share/experiment/experiment1.sh
+++ section_three/dockerfiles/share/experiment/experiment1.sh
@@ -4,4 +4,4 @@ hadoop fs -mkdir -p input
 hadoop fs -put ~/experiment/exp2_sample_data/* input
 hadoop jar ~/experiment/experiment-1.0.jar invertedindex input output 
 rm /root/experiment/result1.txt
-hadoop fs -cat output/* >> /root/experiment/result1.txt
+# hadoop fs -cat output/* >> /root/experiment/result1.txt
