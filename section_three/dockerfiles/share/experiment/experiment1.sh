31bc8116be411037174596ae6c7598ced6758e6c
wanghaogang <whg19962015@163.com>
2018-11-21 05:38:05 +0800

    exp.sh

--- section_three/dockerfiles/share/experiment/experiment1.sh
+++ section_three/dockerfiles/share/experiment/experiment1.sh
@@ -7,10 +7,10 @@
 # hadoop fs -cat output/* >> /root/experiment/result1.txt
 hadoop fs -mkdir -p /data/graphTriangleCount
 hadoop fs -put ~/experiment/twitter_graph_v2.txt  ~/experiment/gplus_combined.unique.txt /data/graphTriangleCount
-hadoop jar ~/experiment/experiment-1.0.jar Triangle_Count /data/graphTriangleCount/twitter_graph_v2.txt output/twitter_ud    
-hadoop jar ~/experiment/experiment-1.0.jar Triangle_Count /data/graphTriangleCount/twitter_graph_v2.txt output/twitter_d 18 directed                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                hadoop jar ~/experiment/experiment-1.0.jar Triangle_Count /data/graphTriangleCount/twitter_graph_v2.txt output/twitter_directed directed
+hadoop jar ~/experiment/experiment-1.0.jar Triangle_Count /data/graphTriangleCount/twitter_graph_v2.txt output/twitter_ud 18
+hadoop jar ~/experiment/experiment-1.0.jar Directed_Triangle_Count /data/graphTriangleCount/twitter_graph_v2.txt output/twitter_d 18
 hadoop jar ~/experiment/experiment-1.0.jar Triangle_Count /data/graphTriangleCount/gplus_combined.unique.txt output/gplus_ud 18
-hadoop jar ~/experiment/experiment-1.0.jar Triangle_Count /data/graphTriangleCount/gplus_combined.unique.txt output/gplus_d 18 directed
+hadoop jar ~/experiment/experiment-1.0.jar Directed_Triangle_Count /data/graphTriangleCount/gplus_combined.unique.txt output/gplus_d 18
 
 # 13082506 1073677742
 # 1818304  27018510
